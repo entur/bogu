ed6bac66f5fec8e696ee5d84962378df1eefc111
Tom Erik Støwer <testower@gmail.com>
2019-11-29 15:14:26 +0100

    Upgrade react and add prop-types

--- src/components/EventStepper.js
+++ src/components/EventStepper.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ActionTranslations from './actionTranslations';
 import FaChevronDown from 'react-icons/lib/fa/chevron-down';
 import FaChevronUp from 'react-icons/lib/fa/chevron-up';
--- src/containers/LineStatsCard.js
+++ src/containers/LineStatsCard.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { Card, CardText } from 'material-ui/Card'
 import { List, ListItem } from 'material-ui/List'
 import IconButton from 'material-ui/IconButton'
